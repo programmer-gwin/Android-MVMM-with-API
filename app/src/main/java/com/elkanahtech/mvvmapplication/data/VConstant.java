4bf454e51f7a7af5bb2fdb3180eb50643c82e83f
GWINA <babsgodwin@gmail.com>
2022-03-03 14:49:50 +0100

    check out

--- app/src/main/java/com/elkanahtech/mvvmapplication/data/VConstant.java
+++ app/src/main/java/com/elkanahtech/mvvmapplication/data/VConstant.java
@@ -1,9 +1,9 @@
 package com.elkanahtech.mvvmapplication.data;
 
 public class VConstant {
-   public static final String BASE_URL = "https://widerpayv3.elkanahtech.com/WiderpayFullJwt/";
-   public static final String LOGIN_ACTION_NAME = "LoginMerchant";
+   public static final String BASE_URL = "https://www.website.com/ctl/";
+   public static final String LOGIN_ACTION_NAME = "Login";
    public static final int LOGIN_CALL_CODE = 100;
-   public static String apiKey="GpUD0/faGgJqfsVV7AmUVzLEa3WdEmHq";
+   public static String apiKey="";
    public static String appId="";
 }
